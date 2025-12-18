d01eede4de8e971046a29e0dbdf850feb1d4d576
Fahim Hossen <fahimnahi2003@gmail.com>
2025-12-18 21:37:29 +0600

    newton raphson and secant basic implementation done

---
+++ Non Linear Equation/NewtonRaphson.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double f(double x) {
+    return x*x - 4.0;
+}
+
+double df(double x) {
+    return 2.0*x;
+}
+
+double NewtonRaphson(double x_n, double epsilon, int max_iteration) {
+    double x_n1;
+    for (int i = 0; i < max_iteration; i++) {
+        x_n1 = x_n - (f(x_n) / df(x_n));
+        if (abs(x_n1 - x_n) < epsilon) {
+            return x_n1;
+        }
+        x_n = x_n1;
+    }
+    return NAN;
+}
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    double range_start, range_end, step, epsilon;
+    int max_iteration;
+    fin >> range_start >> range_end >> step >> epsilon >> max_iteration;
+
+    vector<double> raphson_points;
+    vector<pair<double, double>> secant_pairs;
+
+    for (double i = range_start; i < range_end; i += step) {
+        if (f(i) * f(i + step) < 0) {
+            raphson_points.push_back(i + step / 2);
+            secant_pairs.push_back({i, i + step});
+        }
+    }
+    if (raphson_points.size() == 0) {
+        raphson_points.push_back(0.5);
+    }
+
+    for (double pt : raphson_points) {
+        double root = NewtonRaphson(pt, epsilon, max_iteration);
+        fout << "Root using Newton Raphson: " << root << endl;
+    }
+
+    fin.close();
+    fout.close();
+    return 0;
+}
